cefe240fe864fd757ab0f7732d03a8932ea8f980
Niuxl <45059323+Niuxl074@users.noreply.github.com>
2025-11-17 16:26:22 +0800

    Update detect.py

--- detect.py
+++ detect.py
@@ -4,10 +4,10 @@ from ultralytics import YOLO
 
 if __name__ == '__main__':
     model = YOLO('/home/ps/ultralytics-202404066/ultralytics-main/runs/train/exp_yolov8n-FDPN-LSCD1_nwpu2/weights/best.pt') # select your model.pt path
-    model.predict(source='/home/ps/ultralytics-202404066/ultralytics-main/dataset/NWPUVHR/images/test',
+    model.predict(source='/ESODNet/dataset/DOTA-v1.0/images/test',
                   imgsz=640,
                   project='runs/detect',
-                  name='exp_yolov8n-FDPN-LSCD1_nwpu111',
+                  name='exp_yolov8n-FDPN-LSCD_DOTA-v1.0',
                   save=True,
                   # conf=0.2,
                   # visualize=True # visualize model features maps
