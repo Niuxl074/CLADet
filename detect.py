36a22ddfdbb4e5ee31c5f05d496afaa27b029d61
Niuxl <45059323+Niuxl074@users.noreply.github.com>
2025-11-26 17:08:10 +0800

    Update detect.py

--- detect.py
+++ detect.py
@@ -3,11 +3,11 @@ warnings.filterwarnings('ignore')
 from ultralytics import YOLO
 
 if __name__ == '__main__':
-    model = YOLO('/ESODNet/runs/train/exp_yolov8n-FDPN-LSCD-DOTA-v1.0/weights/best.pt')
-    model.predict(source='/ESODNet/dataset/DOTA-v1.0/images/test',
+    model = YOLO('/CLADet/runs/train/exp_yolov8n-FDPN-LSCD-DOTA-v1.0/weights/best.pt')
+    model.predict(source='/CLADet/dataset/DOTA-v1.0/images/test',
                   imgsz=640,
                   project='runs/detect',
-                  name='exp_yolov8n-FDPN-LSCD_DOTA-v1.0',
+                  name='exp_yolov8n-CLAD-LTFA_DOTA-v1.0',
                   save=True,
                   # conf=0.2,
                   # visualize=True # visualize model features maps
